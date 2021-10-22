b69337e7b39549e17a1fa8c397ae7b0432880536
Sean Washington <hello@seanwash.com>
2021-10-22 15:12:14 -0700

    Add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,16 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+        @env('production')
+            <link rel="icon"
+                  href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤷‍♂️</text></svg>">
+        @endenv
+
+        @env('local')
+            <link rel="icon"
+                  href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🛠</text></svg>">
+        @endenv
+
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
