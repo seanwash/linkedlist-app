33a64ab0329c53b0b78c795093524ddee196be25
Sean Washington <hello@seanwash.com>
2021-10-28 16:01:09 -0700

    Add some basic functionality to the dashboard page
    - Style the list of notes
    - Add a small script that will auto save a note on blur

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,13 +5,21 @@ namespace Database\Seeders;
 use App\Models\Note;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 
 class UserSeeder extends Seeder
 {
     public function run()
     {
         User::factory()
-            ->has(Note::factory(['is_daily' => true])->count(10))
+            ->has(
+                Note::factory(['is_daily' => true])
+                    ->sequence(fn ($sequence) => [
+                        // Generate notes from today to x days in the past, simulating sustained use over multiple days.
+                        'title' => Carbon::now()->subDays($sequence->index)->toFormattedDateString()
+                    ])
+                    ->count(10)
+            )
             ->create(['email' => 'hello@seanwash.com']);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,12 +9,26 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    {{--
+                    TODO: This is a hack to get around tailwind-custom-forms applying
+                          focus styles to all textareas.
+                    --}}
+                    @push('styles')
+                        <style>
+                            textarea:focus {
+                                border: none !important;
+                                outline: none !important;
+                                box-shadow: none !important;
+                            }
+                        </style>
+                    @endpush
+
                     @foreach($notes as $note)
                         <article id="{{ $note->uuid }}" class="py-4 px-2">
-                            <h1 class="text-xl">{{ $note->title }}</h1>
+                            <h1 class="text-xl font-semibold">{{ $note->title }}</h1>
 
                             <form
-                                @keyup.meta.enter="alert('Submitted!')"
+                                id="{{ $note->uuid }}"
                                 method="POST"
                                 action="{{ route('notes.update', $note->uuid) }}"
                             >
@@ -22,19 +36,32 @@
                                 @method('PUT')
                                 <label for="content" class="sr-only">Content</label>
                                 <textarea
-                                    id="content"
+                                    aria-label="Content"
                                     name="content"
-                                    class="mt-2 w-full"
-                                    style="min-height: 300px;"
+                                    class=" mt-2 border-none p-0 w-full"
+                                    style="min-height: 600px;"
                                 >{{ $note->content }}</textarea>
+
                                 @error('title', 'content')
                                 <div>{{ $message }}</div>
                                 @enderror
-
-                                <input type="submit" value="Update">
                             </form>
                         </article>
                     @endforeach
+
+                    @push('scripts')
+                        <script>
+                            let textAreaEls = document.querySelectorAll('textarea');
+
+                            if (textAreaEls.length) {
+                                textAreaEls.forEach(function (textAreaEl) {
+                                    textAreaEl.addEventListener('blur', function (event) {
+                                        this.closest('form').submit();
+                                    });
+                                })
+                            }
+                        </script>
+                    @endpush
                 </div>
             </div>
         </div>
