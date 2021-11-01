665cc1ca2067737cf17705c3cc920f84f6f0a512
Sean Washington <hello@seanwash.com>
2021-11-01 14:58:05 -0700

    Enable js versioning

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         @livewireStyles
         @stack('styles')
 
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
