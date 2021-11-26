9245a1743cfbfc6258f22ad910cef2b1b8d63f2b
Sean Washington <hello@seanwash.com>
2021-11-26 11:29:51 -0800

    Add conditional around Inertia creation…
    so that the app falls back to blade for a specific subset of pages.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,28 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/inertia-svelte';
 import { InertiaProgress } from '@inertiajs/progress';
 
-InertiaProgress.init();
+console.log(window.location.pathname);
 
-createInertiaApp({
-  resolve: (name) => require(`./pages/${name}.svelte`),
-  setup({ el, App, props }) {
-    new App({ target: el, props });
-  },
-}).catch((error) => {
-  alert(error.message);
-});
+// Skip booting the Inertia app if the current page is any of the auth routes.
+// We haven't converted the auth routes to using inertia yet.
+const shouldCreateInertiaApp = [
+  'login',
+  'register',
+  'forgot-password',
+  'reset-password',
+  'confirm-password',
+  'verify-email',
+].every((value) => !window.location.pathname.includes(value));
+
+if (shouldCreateInertiaApp) {
+  InertiaProgress.init();
+
+  createInertiaApp({
+    resolve: (name) => require(`./pages/${name}.svelte`),
+    setup({ el, App, props }) {
+      new App({ target: el, props });
+    },
+  }).catch((error) => {
+    alert(error.message);
+  });
+}
